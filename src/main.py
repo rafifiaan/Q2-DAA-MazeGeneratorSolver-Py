a2815e4487b17f5ce0f54ee4160ea902bbb5d7f8
rafifiaan <rafialiefian03@gmail.com>
2023-05-18 01:46:59 +0700

    add exp funct on main program

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from tkinter import Canvas, Button, StringVar, LEFT, RIGHT, OptionMenu, Tk
 from generate_maze import *
 
-
+# Frame Maze
 class Window:
     def __init__(self, master, width, height):
         self.app = master
@@ -19,6 +19,7 @@ class Window:
         self.create_maze_screen()
         self.create_buttons()
 
+    # Interface Screen after running
     def create_maze_screen(self):
         self.maze_screen = Canvas(
             self.app, width=self.width,
@@ -26,6 +27,7 @@ class Window:
         )
         self.maze_screen.pack(fill="both", expand=True)
 
+    # Button choice
     def create_buttons(self):
         self.generate_button = Button(
             self.app, text='Generate maze!', command=self.draw_maze)
@@ -41,6 +43,7 @@ class Window:
     def get_selected_algorithm(self, _=None):
         return self.algorithm.get()
 
+    # Rendering maze
     def draw_maze(self):
         self.generate_button.config(state='disabled')
         self.maze_screen.delete("all")
